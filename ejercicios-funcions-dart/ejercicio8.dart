5f35941d7968118c9e3e16db43c1c4e3bd4a0506
Eduardo González Expósito <131476025+eduglezexp@users.noreply.github.com>
2025-09-24 16:21:50 +0100

    Delete ejercicios-funcions-dart directory

--- ejercicios-funcions-dart/ejercicio8.dart
+++
@@ -1,5 +0,0 @@
-/**
- * Escribe una funcion que genere una lista 
- * con los cuadrados de los primeros n 
- * numeros enteros. Usa una funcion flecha (=>).
- */
--- ejercicios-funcions-dart/ejercicio9.dart
+++
@@ -1,9 +0,0 @@
-/**
- * Ejercicio 9: Crea una funcion que devuelva 
- * otra funcion (closure). La funcion devuelta 
- * debe multiplicar por un factor fijo el 
- * numero que reciba.
- * Ejemplo:
- * var porTres = multiplicador(3);
- * print(porTres(5)); // 15
- */
